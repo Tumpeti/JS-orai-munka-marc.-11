66c5c95b61fe979429547491033e3dadf9492fb6
Tumpeti <petertumpek20@gmail.com>
2023-03-11 11:19:36 +0100

    Kódok alaki formázása

--- script.js
+++ script.js
@@ -8,36 +8,37 @@ const KEPEK = [
     "./kepek/Kep5.jpg"]
 
 function init() {
-    
+
 
     let nagykep = document.querySelector(".nagykep img");
     let article = document.querySelector("article")
     let htmlValtozo = kepethozzaad(KEPEK);
-    kirak(htmlValtozo,article)
+    kirak(htmlValtozo, article)
     let kiskepek = document.querySelectorAll(".kiskep img")
 
 
-    for (let i = 0; i < kiskepek.length; i++){
-        kiskepek[i].addEventListener("click",function(){
+    for (let i = 0; i < kiskepek.length; i++) {
+        kiskepek[i].addEventListener("click", function () {
             kattintasKezelo(i)
         });
     }
 
 }
 
-function kepethozzaad(KEPEK){
+function kepethozzaad(KEPEK) {
     let htmlValtozo = `<div class="kiskep">`;
-    for (let i =0; i < KEPEK.length; i++){
-    htmlValtozo += `<img src="${KEPEK[i]}"`;
-    htmlValtozo += "</div>";}
+    for (let i = 0; i < KEPEK.length; i++) {
+        htmlValtozo += `<img src="${KEPEK[i]}"`;
+        htmlValtozo += "</div>";
+    }
     return htmlValtozo
 }
 
-function kirak(mit, hova){
+function kirak(mit, hova) {
     hova.innerHTML += mit;
 }
 
-function kattintasKezelo(i, event){
+function kattintasKezelo(i, event) {
     console.log(event.target)
     console.log(event.target.src)
     const NAGYKEP = document.querySelector(".nagykep img");
